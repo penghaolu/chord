7e982f2fb57c5bc2ee8349fb72273bdcbeb0ab71
Peng Hao Lu <pengluh@gmail.com>
2023-03-12 22:40:58 -0700

    init page structure

---
+++ .next/server/pages/select-friends.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/select-friends";
+exports.ids = ["pages/select-friends"];
+exports.modules = {
+
+/***/ "./src/pages/select-friends/index.js":
+/*!*******************************************!*\
+  !*** ./src/pages/select-friends/index.js ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ SelectFriends)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction SelectFriends() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        children: [\n            \"hello\",\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                children: \"testing friend select\"\n            }, void 0, false, {\n                fileName: \"/Users/penghaolu/Documents/GitHub/chord/src/pages/select-friends/index.js\",\n                lineNumber: 5,\n                columnNumber: 7\n            }, this)\n        ]\n    }, void 0, true, {\n        fileName: \"/Users/penghaolu/Documents/GitHub/chord/src/pages/select-friends/index.js\",\n        lineNumber: 3,\n        columnNumber: 5\n    }, this);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvcGFnZXMvc2VsZWN0LWZyaWVuZHMvaW5kZXguanMuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUFlLFNBQVNBLGdCQUFnQjtJQUN0QyxxQkFDRSw4REFBQ0M7O1lBQUk7MEJBRUgsOERBQUNDOzBCQUFHOzs7Ozs7Ozs7Ozs7QUFHVixDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vc3BvdGlmeS1uZXR3b3JrLy4vc3JjL3BhZ2VzL3NlbGVjdC1mcmllbmRzL2luZGV4LmpzP2YyMjgiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gU2VsZWN0RnJpZW5kcygpIHtcbiAgcmV0dXJuIChcbiAgICA8ZGl2PlxuICAgICAgaGVsbG9cbiAgICAgIDxoMT50ZXN0aW5nIGZyaWVuZCBzZWxlY3Q8L2gxPlxuICAgIDwvZGl2PlxuICApO1xufVxuIl0sIm5hbWVzIjpbIlNlbGVjdEZyaWVuZHMiLCJkaXYiLCJoMSJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/pages/select-friends/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./src/pages/select-friends/index.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ src/pages/select-friends/index.js
@@ -0,0 +1,8 @@
+export default function SelectFriends() {
+  return (
+    <div>
+      hello
+      <h1>testing friend select</h1>
+    </div>
+  );
+}
